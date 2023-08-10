c163045983f0c93bbd2abb4a9b0eac4bd638a6fc
FredericoGSilva <frederico.dev@outlook.com>
2023-08-10 20:50:38 -0300

    AuthenticationContext.ts: AuthenticationProvider function created.

--- src/data/contexts/AuthenticationContexts.tsx
+++ src/data/contexts/AuthenticationContexts.tsx
@@ -1,5 +1,6 @@
 import User from "@/logic/core/user/User"
-import { createContext } from "react"
+import AuthenticationFirebase from "@/logic/firebase/authentication/Authentication"
+import { createContext, useState, useEffect } from "react"
 
 interface Authentication {
     loading: boolean
@@ -15,4 +16,40 @@ const AuthenticationContext = createContext<Authentication>({
     logout: async () => {}
 })
 
-export default AuthenticationContext
+export default AuthenticationContext 
+
+export function AuthenticationProvider(props: any) {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [user, setUser] = useState<User | null>(null)
+    const classAuthentication = new AuthenticationFirebase()
+
+    useEffect(() => {
+        const cancel = classAuthentication.toMonitor(function(user) {
+            setUser(user)
+            setLoading(false)
+        })
+        return () => cancel()
+    })
+
+    async function loginGoogle(): Promise<User | null> {
+        const user = await classAuthentication.loginGoogle()
+        setUser(user)
+        return user
+    }
+
+    async function logout(): Promise<void> {
+        await classAuthentication.logout()
+        setUser(null)
+    }
+
+    return (
+        <AuthenticationContext.Provider value={{
+            loading,
+            user,
+            loginGoogle,
+            logout
+        }}>
+            {props.children}
+        </AuthenticationContext.Provider>
+    )
+}
